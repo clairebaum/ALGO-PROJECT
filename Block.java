23193ef1f73f271ed7a14cd4267c624d0e0ac444
clairebaum <100951963+clairebaum@users.noreply.github.com>
2022-03-04 17:50:39 +0100

    Create Block.java

---
+++ Block.java
@@ -0,0 +1,17 @@
+import javax.awt.*;
+
+public abstract class Block{
+
+	protected int posX;
+	protected int posY;
+	protected int height;
+	protected int width;
+	
+	public Block(int x, int y , int height, int width){
+		this.posX = x;
+		this.posY = y;
+		this.height = height;
+		this.width = width ; 
+	}
+	
+	public abstract paint(Graphics g);
