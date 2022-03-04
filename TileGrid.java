98609348b4ff9f9f8959a9d3744db7e9ea933cdd
clairebaum <100951963+clairebaum@users.noreply.github.com>
2022-03-04 17:50:16 +0100

    Create TileGrid.java

---
+++ TileGrid.java
@@ -0,0 +1,12 @@
+public class TileGrid{
+
+	protected Tile[][] map;
+	
+		map = new Tile[20][20];
+		for(int i = 0; i<map.length; i++){
+			for(int j =0 ; j<map[i].length, j++){
+				map[][] = new Tile(i*10 , j*10, i, j);
+			}
+		}
+	}
+}
