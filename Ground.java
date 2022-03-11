37b3d84f69709760f9b1c0a98069f6c01bdb5f7b
clairebaum <100951963+clairebaum@users.noreply.github.com>
2022-03-11 16:52:17 +0100

    Create Ground.java

---
+++ Ground.java
@@ -0,0 +1,17 @@
+import javax.awt.*;
+
+
+public class Ground extends Tile{
+	
+	public Ground(int x, int y , int height, int width){
+	super(x, y, height, width); 
+	}
+	
+	public void paint(Graphics g){
+		g.setColor(color.green);
+		g.drawRect(this.posX, this.posY, this.height, this.width);
+		
+	}
+	
+	
+}
