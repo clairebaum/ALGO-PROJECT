828574df580376594e0906dc9dd78a1fa1f41a4e
clairebaum <100951963+clairebaum@users.noreply.github.com>
2022-03-18 16:54:23 +0100

    Update TileGrid.java

--- TileGrid.java
+++ TileGrid.java
@@ -1,13 +1,22 @@
+
 public class TileGrid{
 
 	protected Tile[][] map;
 	
-		map = new Tile[20][20];
+		map = new Tile[30][30];
 		for(int i = 0; i<map.length; i++){
 			for(int j =0 ; j<map[i].length, j++){
-				map[][] = new Ground(i*10 , j*10, i, j);
+				map[][] = new Tile(i*10 , j*10, 10, 10);
 			}
 		}
 	}
+	
+	protected void draw(){
+		for(int i = 0; i<map.length; i++){
+			for(int j =0 ; j<map[i].length, j++){
+				Tile t = map[i][j];
+				t.paint();
+			}
+		}
+	}		
 }
-//we still have to add the Walls and add the centre "win" tile
