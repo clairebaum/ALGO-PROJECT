3ff33a3fcab5efb74aa1f6d23107af55599441ff
clairebaum <100951963+clairebaum@users.noreply.github.com>
2022-03-18 18:18:24 +0100

    Update frameGame.java

--- frameGame.java
+++ frameGame.java
@@ -2,17 +2,16 @@ import javax.swing.*;
 import java.awt.Color;
 import java.awt.event.*;
 
-public class frameGame extends JFrame implements ActionListener{
+
+public class frameGame extends JFrame implements ActionListener, KeyListener{
 	// attributes of class frameGame
 	public JPanel mainLabel;
 	public JPanel grid;
 	public JButton throwDice;
 	public JButton moveWall;
-    	public Tile[][] map;
 	
-	public frameGame(String name, int width, int height, Tile[][] map) {
+	public frameGame(String name, int width, int height) {
 		super(name);
-        	this.map=map;
 		setSize(width, height);
 		setLocation(200,50);
 
@@ -27,6 +26,7 @@ public class frameGame extends JFrame implements ActionListener{
 		moveWall= new JButton("Move Wall");
 		moveWall.setBounds(10,260,150,40);
 		moveWall.addActionListener(this);
+		moveWall.addKeyListener(this);
 
 		// JPanel that will contain the grid
 		grid=new JPanel();
@@ -42,16 +42,41 @@ public class frameGame extends JFrame implements ActionListener{
 		mainLabel.add(grid);
 		mainLabel.setBounds(0,0,800,600);
 		mainLabel.setBackground(Color.YELLOW);
+		
 
 		add(mainLabel);
 
 		setVisible(true);
+		setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
 
 
-	}
-	public void paint(Graphics g){
-		tileGrid.draw();
 	}
 	
+	public void actionPerformed(ActionEvent e){
+		moveWall.setBackground(Color.MAGENTA);
+	}
+	
+	public void keyPressed(KeyEvent ke){
+		System.out.println();
+	}
+	
+	public void keyReleased(KeyEvent ke){
+		if (ke.getKeyCode() == KeyEvent.VK_LEFT){
+			moveWall.setBackground(Color.MAGENTA);
+			
+		}
+		else if(ke.getKeyCode() == KeyEvent.VK_UP){
+			moveWall.setBackground(Color.BLACK);
+		}
+		else {
+			moveWall.setBackground(Color.GREEN);
+		}
+		repaint();
+	}
+	
+	public void keyTyped(KeyEvent ke){
+		System.out.println();
+	}
 }
 
