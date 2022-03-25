52a6c198c5a1fc0a20b27e2fc219452b28a7bf42
Emelinehehehe <100952122+Emelinehehehe@users.noreply.github.com>
2022-03-25 16:25:34 +0100

    Update frameGame.java

--- frameGame.java
+++ frameGame.java
@@ -86,3 +86,96 @@ public class frameGame extends JFrame implements ActionListener, KeyListener{
 		this.theTileGrid.draw();
 	}
 }
+
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.event.*;
+import javax.swing.ImageIcon;
+
+
+public class frameGame extends JFrame implements ActionListener, KeyListener{
+	// attributes of class frameGame
+	public JPanel mainLabel;
+	public JPanel grid;
+	public JButton throwDice;
+	public JButton moveWall;
+	private ImageIcon HeroIcon;
+    private JLabel HeroLabel;
+	
+	public frameGame(String name, int width, int height) {
+		super(name);
+		setSize(width, height);
+		setLocation(200,50);
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// button to throw the dice
+		throwDice= new JButton("Throw Dice");
+		throwDice.setBounds(10,200,150,40);
+		throwDice.addActionListener(this);
+
+		// button to move a wall
+		moveWall= new JButton("Move Wall");
+		moveWall.setBounds(10,260,150,40);
+		moveWall.addActionListener(this);
+		moveWall.addKeyListener(this);
+
+		//this doesn't work, problem with image icon
+		Hero hero1= new Hero (15,15);
+		HeroIcon= new ImageIcon(getClass().getResource("avatar.jpg"));
+        HeroLabel= new JLabel(HeroIcon);
+        HeroLabel.setSize(20,20);
+		HeroLabel.setLocation(15,15);
+
+		// JPanel that will contain the grid
+		grid=new JPanel();
+		grid.setLayout(null);
+		grid.setBounds(200, 60, 500,400);
+		grid.setBackground(Color.CYAN);
+		grid.add(hero1);
+
+		// JPanel that will contain everything (grid, buttons...)
+		mainLabel= new JPanel();
+		mainLabel.setLayout(null);
+		mainLabel.add(throwDice);
+		mainLabel.add(moveWall);
+		mainLabel.add(grid);
+		mainLabel.setBounds(0,0,800,600);
+		mainLabel.setBackground(Color.YELLOW);
+		
+
+		add(mainLabel);
+
+		setVisible(true);
+		setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+
+
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		moveWall.setBackground(Color.MAGENTA);
+	}
+	
+	public void keyPressed(KeyEvent ke){
+		System.out.println();
+	}
+	
+	public void keyReleased(KeyEvent ke){
+		if (ke.getKeyCode() == KeyEvent.VK_LEFT){
+			moveWall.setBackground(Color.MAGENTA);
+			
+		}
+		else if(ke.getKeyCode() == KeyEvent.VK_UP){
+			moveWall.setBackground(Color.BLACK);
+		}
+		else {
+			moveWall.setBackground(Color.GREEN);
+		}
+		repaint();
+	}
+	
+	public void keyTyped(KeyEvent ke){
+		System.out.println();
+	}
+}	
