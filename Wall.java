6990ed3cfa5e4020dac82eccaed24a668a946e78
Eleonore12345 <77687565+Eleonore12345@users.noreply.github.com>
2022-04-01 20:21:21 +0200

    Create Wall.java

--- Wall.java
+++ Wall.java
@@ -1,6 +1,5 @@
 import java.awt.*;
 
-
 public class Wall extends Tile{
 	
 	public Wall(int x, int y , int height, int width){
@@ -8,7 +7,9 @@ public class Wall extends Tile{
 	}
 	
 	public void paint(Graphics g){
-		g.setColor(color.black);
+		g.setColor(Color.BLACK);
+		g.fillRect(this.posX, this.posY, this.height, this.width);
+		g.setColor(Color.GREEN);
 		g.drawRect(this.posX, this.posY, this.height, this.width);
 		
 	}
