a39e66c42333ca6aa69d3fff771aded27a2cbb23
clairebaum <100951963+clairebaum@users.noreply.github.com>
2022-04-01 16:51:01 +0200

    Update UFO.java

--- UFO.java
+++ UFO.java
@@ -3,6 +3,7 @@ public class UFO extends Character {
 
     public UFO (int xini,int yini){ 
         super(xini,yini);
+        this.player = 2;
     }
 
     public void winOrLose (int xHero, int yHero){
