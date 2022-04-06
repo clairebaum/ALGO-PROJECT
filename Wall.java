0e7cbd506cdbefbbbd77c483e5b2413e985cb51b
Emelinehehehe <100952122+Emelinehehehe@users.noreply.github.com>
2022-04-06 13:27:45 +0200

    Update Wall.java

--- Wall.java
+++ Wall.java
@@ -7,9 +7,9 @@ public class Wall extends Tile{
 	}
 	
 	public void paint(Graphics g){
-		g.setColor(Color.BLACK);
+		g.setColor(new Color(46,46,31));
 		g.fillRect(this.posX, this.posY, this.height, this.width);
-		g.setColor(Color.GREEN);
+		g.setColor(new Color(0,0,0));
 		g.drawRect(this.posX, this.posY, this.height, this.width);
 		
 	}
