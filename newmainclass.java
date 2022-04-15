ebb70b00929a13db44c2bae490d6c2a03fed73ea
clairebaum <100951963+clairebaum@users.noreply.github.com>
2022-04-15 14:19:33 +0200

    Create newmainclass.java

---
+++ newmainclass.java
@@ -0,0 +1,7 @@
+public class newmainclass{
+	public static void main(String[] args){
+	private frameGame mainFrame = new frameGame(Name, 800, 600); //creates the game window
+	newGame game = new newGame(); 
+	game.start(); //launches the game
+	}
+}
