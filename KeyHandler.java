199dd729e497cd79bb96f481ef72de38f3eac82e
Eleonore12345 <77687565+Eleonore12345@users.noreply.github.com>
2022-04-20 21:54:21 +0200

    Update KeyHandler.java

--- KeyHandler.java
+++ KeyHandler.java
@@ -1,109 +1,67 @@
 import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
 
+/*changed a lot: the detection of the keys pressed was only done when the thread was actualising 
+hence some signals were not taken into account. Here the methods are static because there is only one KeyHandler
+ -> the method does not depend on the object
+*/
 // implementation of the links between the keyboard anf the action
 
 public class KeyHandler implements KeyListener {
 
-  public boolean pressed, upPressed, leftPressed, downPressed, rightPressed, zPressed, qPressed, sPressed, dPressed;
+  public static long lastUpTime, lastDownTime, lastLeftTime, lastRightTime,
+                      lastZTime, lastDTime, lastSTime, lastQTime;
 
-	
-  
-  public void keyTyped(KeyEvent e) {
+  public static long[] keyTimes = new long[256]; //array storing the last time the key was pressed at the position corresponding to its ascii code
 
-  }
+  public void keyTyped(KeyEvent e) {}
 
   public void keyPressed(KeyEvent e) {
-	pressed = true; 
-	switch(e.getKeyCode()) {
-      case KeyEvent.VK_Z:
-        zPressed = true;
-        break;
-      case KeyEvent.VK_Q:
-        qPressed = true;
-        break;
-      case KeyEvent.VK_S:
-        sPressed = true;
-        break;
-      case KeyEvent.VK_D:
-        dPressed = true;
-        break;
-      case KeyEvent.VK_UP:
-        upPressed = true;
-        break;
-      case KeyEvent.VK_LEFT:
-        leftPressed = true;
-        break;
-      case KeyEvent.VK_DOWN:
-        downPressed = true;
-        break;
-      case KeyEvent.VK_RIGHT:
-        rightPressed = true;
-        break;
-
-    }
+    keyTimes[e.getKeyCode()] = System.currentTimeMillis(); //updates the value of the time at the posiiton of the pressed key 
   }
 
   @Override
-  public void keyReleased(KeyEvent e) {
-	  pressed = false
-    switch(e.getKeyCode()) {
-      case KeyEvent.VK_Z:
-        zPressed = false;
-        break;
-      case KeyEvent.VK_Q:
-        qPressed = false;
-        break;
-      case KeyEvent.VK_S:
-        sPressed = false;
-        break;
-      case KeyEvent.VK_D:
-        dPressed = false;
-        break;
-      case KeyEvent.VK_UP:
-        upPressed = false;
-        break;
-      case KeyEvent.VK_LEFT:
-        leftPressed = false;
-        break;
-      case KeyEvent.VK_DOWN:
-        downPressed = false;
-        break;
-      case KeyEvent.VK_RIGHT:
-        rightPressed = false;
-        break;
-   
-    }
-  }
-  
-   public void keyReleased(KeyEvent e) {
-    switch(e.getKeyCode()) {
-      case KeyEvent.VK_Z:
-        zPressed = false;
-        break;
-      case KeyEvent.VK_Q:
-        qPressed = false;
-        break;
-      case KeyEvent.VK_S:
-        sPressed = false;
-        break;
-      case KeyEvent.VK_D:
-        dPressed = false;
-        break;
-      case KeyEvent.VK_UP:
-        upPressed = false;
-        break;
-      case KeyEvent.VK_LEFT:
-        leftPressed = false;
-        break;
-      case KeyEvent.VK_DOWN:
-        downPressed = false;
-        break;
-      case KeyEvent.VK_RIGHT:
-        rightPressed = false;
-        break;
 
+  public void keyReleased(KeyEvent e) {}
+
+  private static final int LOOP_DELAY = 200; //corresponds to the actualising rate of the thread
+
+  public static boolean isKeyPressed(int key) { //tests if the given key has just been pressed and put its time to zero
+    if(System.currentTimeMillis() - keyTimes[key] <= LOOP_DELAY) {
+      keyTimes[key] = 0;
+      return true;
     }
+    return false;
   }
+
+  public static boolean isUpPressed() { //keyPressed is used for each key 
+    return isKeyPressed(KeyEvent.VK_UP);
+  }
+
+  public static boolean isDownPressed() {
+    return isKeyPressed(KeyEvent.VK_DOWN);
+  }
+  public static boolean isRightPressed() {
+    return isKeyPressed(KeyEvent.VK_RIGHT);
+  }
+  public static boolean isLeftPressed() {
+    return isKeyPressed(KeyEvent.VK_LEFT);
+  }
+  public static boolean isZPressed() {
+    return isKeyPressed(KeyEvent.VK_Z);
+  }
+
+  public static boolean isSPressed() {
+    return isKeyPressed(KeyEvent.VK_S);
+  }
+
+  public static boolean isQPressed() {
+    return isKeyPressed(KeyEvent.VK_Q);
+  }
+
+  public static boolean isDPressed() {
+    return isKeyPressed(KeyEvent.VK_D);
+  }
+
 
 }
