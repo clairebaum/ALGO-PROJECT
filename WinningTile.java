c2fafa349dc1c2b7f21b1895599734c0df07b499
Emelinehehehe <100952122+Emelinehehehe@users.noreply.github.com>
2022-04-24 21:14:22 +0200

    Update and rename WinningTile to WinningTile.java

--- WinningTile.java
+++ WinningTile.java
@@ -8,7 +8,7 @@ public class WinningTile extends Ground {
 	public void paint(Graphics g){
 		g.setColor(Color.BLACK);
 		g.fillRect(this.posX, this.posY, this.height, this.width);
-		g.setColor(Color.blue);
+		g.setColor(new Color(255,0,0));
 		g.drawRect(this.posX, this.posY, this.height, this.width);
 		
 	}	
