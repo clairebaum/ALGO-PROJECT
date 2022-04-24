72c110c559e6e6016701a0b1d37d92021398e517
Emelinehehehe <100952122+Emelinehehehe@users.noreply.github.com>
2022-04-24 21:09:28 +0200

    Update Ground.java

--- Ground.java
+++ Ground.java
@@ -8,9 +8,9 @@ public class Ground extends Tile{
 	}
 	
 	public void paint(Graphics g){
-		g.setColor(new Color (255,179,102));
+		g.setColor(new Color (198, 140, 83));
 		g.fillRect(this.posX, this.posY, this.height, this.width);
-		g.setColor(new Color (153,77,0));
+		g.setColor(new Color (135, 89, 44));
 		g.drawRect(this.posX, this.posY, this.height, this.width);
 		
 	}
