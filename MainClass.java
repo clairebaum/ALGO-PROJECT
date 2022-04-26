45224cc608968214b21298bb4b523b43a3721901
Eleonore12345 <77687565+Eleonore12345@users.noreply.github.com>
2022-04-26 21:28:16 +0200

    Update and rename main.java to MainClass.java

---
+++ MainClass.java
@@ -0,0 +1,12 @@
+public class MainClass{
+	
+	public static FrameMenu menuFrame;
+	
+	public static void main(String[] args){
+    String Name = "UFO Attack";
+    
+    menuFrame = new FrameMenu (Name, 1000, 800);
+	
+	
+	}
+}
