4f0708d8703cdc0852a604f8d486c2e8afdeb6cd
Eleonore12345 <77687565+Eleonore12345@users.noreply.github.com>
2022-04-26 21:20:43 +0200

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -1,19 +1,15 @@
 public class Hero extends Character {
 
-    // for now there aren't any additional attribute but we can add them here and in the constructor
-
     public Hero (int xini,int yini){ 
         super(xini,yini);
         this.player = 1 ;
     }
-    //I didn't override displacement because no additional feature 
 
     public void winOrLose (){
-        if (this.x==3 && this.y==3){ //change 3 to the centre coordinates!!!
+        if (this.x>12 && this.x<17 && this.y>12 && this.y<17){ //change 3 to the centre coordinates!!!
             this.hasWinned = true;
+            System.out.println("The hero has winned!");
+        }
     }
-    //in the main we should call a method endGame(boolean 1 or boolean 2) {} 
-    //where 1 and 2 are nameHero.hasWinned and nameUFO.hasWinned
 
 }
-
