ed1dc029d0ef91ceb44dcd86065b5c6a4d43cfc8
Eleonore12345 <77687565+Eleonore12345@users.noreply.github.com>
2022-04-26 21:19:10 +0200

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,85 +1,106 @@
-public class Character implements KeyHandler{
-    int x;
-    int y;
-    int availableDisplacement;
-    boolean hasWinned;
-    KeyHandler control;
-    int player ; 
-    boolean isPlaying;
-    boolean upPressed, downPressed, leftPressed, rightPressed, pressed ; 
-    
-    
+public class Character {
+	int x;
+	int y;
+	int availableDisplacement;
+	int availableWallMoving;
+	boolean hasWinned;
+	int player;
+	boolean upPressed, downPressed, leftPressed, rightPressed;
 
-    public Character(int xini,int yini){ //constructor setting the initial position
-        this.x=xini;
-        this.y=yini;
-        this.availableDisplacement = 0;
-        this.hasWinned = false;
-        this.control = control; 
-        this.isPlaying= false ;
-        
-    }
-    
-    public int getX() {
+	public Character(int xini, int yini) { // constructor setting the initial position
+		this.x = xini;
+		this.y = yini;
+		this.availableDisplacement = 0;
+		this.hasWinned = false;
+		this.availableWallMoving = 0;
+
+	}
+
+	public int getX() {
 		return this.x;
 	}
-	
+
 	public int getY() {
 		return this.y;
 	}
 
-    /*public void displacement (int horizontal, int vertical){ //will work with Actionlistener and takes values 0, 1 or -1
-        // must check before calling displacement if there is a Wall (tileGrid[x/y +ou- 1]) depending on the arrow pressed
-        
-        this.x += horizontal*10;
-        this.y += vertical*10;
-    }
-    //after calling method displacement we must update the graphical interface
-    */
-    public void updatePosition() {
 	
-		switch{
-			case upPressed:
+	public boolean updatePosition() {
+		boolean somethingChanged = false; //the boolean that will be returned
+		if (upPressed) {
+			if (getY() > 0 && MainPanel.theTileGrid.binaryMap[getX()][getY() - 1] != 1) { //tests if it's a wall 
+				this.y += -1; //upPressed corresponds to minus 1 because the scale is downwards
+				somethingChanged = true;
+			}
+		} else if (downPressed) {
+			if (getY() < MainPanel.theTileGrid.binaryMap[0].length-1 && MainPanel.theTileGrid.binaryMap[getX()][getY() + 1] != 1) {
 				this.y += 1;
-			break ;
-			case downPressed :
-				this.y += -1;
-			case rightPressed:
+				somethingChanged = true;
+			}
+		} else if (rightPressed) {
+			if (getX() < MainPanel.theTileGrid.binaryMap.length-1 && MainPanel.theTileGrid.binaryMap[getX() + 1][getY()] != 1) {
 				this.x += 1;
-			break ;
-			case downPressed :
-				this.x += -1;	
-			break;
-		}
+				somethingChanged = true;
+			}
+		} else if (leftPressed) {
+			if (getX() > 0 && MainPanel.theTileGrid.binaryMap[getX() - 1 ][getY()] != 1){
+				this.x -= 1;
+				somethingChanged = false;
+			}
+		}	
+		return somethingChanged;	
 	}
-	
-	public void KeyPressed() {
-		
-	upPressed = false;
-	downPressed = false;
-	leftPressed = false;
-	rightPressed = false;
-	pressed = false ;
-	
-		if (canMove){ // check if player is allowed to move
-		pressed = control.pressed;
-		
-			if(player == 1 && pressed) { // controls for Hero
-			
-			upPressed = control.zPressed;
-			downPressed = control.sPressed;
-			leftPressed = control.qPressed;
-			rightPressed = control.dPressed;
-		  
-			} else if(player == 2 && pressed) { // controls for UFO
-			 
-			upPressed = control.upPressed;
-			downPressed = control.downPressed;
-			leftPressed = control.leftPressed;
-			rightPressed = control.rightPressed;
-		  
+
+
+	public boolean updateWalls (){
+		boolean somethingChanged = false;
+		if(MainPanel.recentlyClicked){
+			int x = (int)((MainPanel.mouseX-200)/20);
+			int y = (int)((MainPanel.mouseY-20)/20);
+			System.out.println(x+" "+y);
+			System.out.println("firstSelected: " + MainPanel.firstSelected);
+
+			if(0<=x && x<=30 && y>=0 && y<=30 && !MainPanel.firstSelected){
+				System.out.println("entrée dans la boucle de conditions de coordonnées first");
+				if(MainPanel.theTileGrid.binaryMap[x][y] ==1){
+					System.out.println("entrée dans la boucle de condition binaryMap first");
+					MainPanel.theTileGrid.binaryMap[x][y]=0;
+					MainPanel.theTileGrid.map[x][y]=new Ground(20*x+200,20*y+20,20,20);
+					MainPanel.firstSelected=true;
+					System.out.println("recentlyClicked: " + MainPanel.recentlyClicked);
+				}
+			} else if(0<=x && x<=30 && y>=0 && y<=30 && MainPanel.firstSelected){
+				System.out.println("entrée dans la boucle de conditions de coordonnées second"); 
+				if(MainPanel.theTileGrid.binaryMap[x][y] ==0){
+					System.out.println("entrée dans la boucle de condition binaryMap second");
+					MainPanel.theTileGrid.binaryMap[x][y]=1;
+					MainPanel.theTileGrid.map[x][y]=new Wall(20*x+200,20*y+20,20,20);
+					MainPanel.firstSelected=false;
+					somethingChanged = true;
+				}
+
+				
+			} else {
+				System.out.println("You clicked on the side!");
 			}
 		}
+		return somethingChanged;
+	}
 
+	public void KeyPressed(){
+		if(player == 1) { // controls for Hero
+			
+			upPressed = KeyHandler.isUpPressed();
+			downPressed = KeyHandler.isDownPressed();
+			leftPressed = KeyHandler.isLeftPressed();
+			rightPressed = KeyHandler.isRightPressed();
+		  
+		} else if(player == 2) { // controls for UFO
+			 
+			upPressed = KeyHandler.isZPressed();
+			downPressed = KeyHandler.isSPressed();
+			leftPressed = KeyHandler.isQPressed();
+			rightPressed = KeyHandler.isDPressed();
+		}
 	}
 }
