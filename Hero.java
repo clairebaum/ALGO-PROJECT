b16f19667a0163feb05bd3df8ce3e007593504c5
clairebaum <100951963+clairebaum@users.noreply.github.com>
2022-05-04 00:13:41 +0200

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -6,7 +6,7 @@ public class Hero extends Character {
     }
 
     public void winOrLose (){
-        if (this.x>12 && this.x<17 && this.y>12 && this.y<17){ //center coordinates
+        if (this.x>13 && this.x<17 && this.y>13 && this.y<17){ //center coordinates
             this.hasWon = true;
             System.out.println("The hero has won!");
         }
