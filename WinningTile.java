0763d6093b63a9d4f19e4d85620bb22e0a24841b
Eleonore12345 <77687565+Eleonore12345@users.noreply.github.com>
2022-05-04 21:20:04 +0200

    Update WinningTile.java

--- WinningTile.java
+++ WinningTile.java
@@ -1,11 +1,16 @@
 import java.awt.*;
 
 public class WinningTile extends Ground {	
+	
+	/** Constructor
+	  */
 	public WinningTile (int x, int y , int height, int width){
 	super(x, y, height, width); 
 	}
 	
-	public void paint(Graphics g){
+	/** paint method, overriden from Ground
+	  */
+	public void paint(Graphics g){ //draws a square with orange contour at the position of the WinningTile (in the center of the maze)
 		g.setColor(Color.BLACK);
 		g.fillRect(this.posX, this.posY, this.height, this.width);
 		g.setColor(new Color(255,0,0));
