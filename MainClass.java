3901129c1039c6dfe7fbf2281fa96cfbb21a3e0f
Eleonore12345 <77687565+Eleonore12345@users.noreply.github.com>
2022-05-04 21:16:14 +0200

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -1,12 +1,11 @@
 public class MainClass{
 	
 	public static FrameMenu menuFrame;
-	
+	/** Main method, redirects to FrameMenu
+	  */
 	public static void main(String[] args){
     String Name = "UFO Attack";
-    
-    menuFrame = new FrameMenu (Name, 1000, 800);
-	
-	
+    menuFrame = new FrameMenu (Name, 1000, 800); 
 	}
+
 }
