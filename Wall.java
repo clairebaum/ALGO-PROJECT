76c9e60f85be68a83019a51d7ccd9de98ff42277
Eleonore12345 <77687565+Eleonore12345@users.noreply.github.com>
2022-05-04 21:19:34 +0200

    Update Wall.java

--- Wall.java
+++ Wall.java
@@ -1,12 +1,15 @@
 import java.awt.*;
 
 public class Wall extends Tile{
-	
+
+	/** Constructor
+	  */
 	public Wall(int x, int y , int height, int width){
 	super(x, y, height, width); 
 	}
-	
-	public void paint(Graphics g){
+	/** paint method
+	  */	
+	public void paint(Graphics g){ //draws a dark square and its contour at the position of the wall
 		g.setColor(new Color(46,46,31));
 		g.fillRect(this.posX, this.posY, this.height, this.width);
 		g.setColor(new Color(0,0,0));
