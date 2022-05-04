a66b7015d04dbbf6ea3bdbb3af53bd2c040fa21f
Eleonore12345 <77687565+Eleonore12345@users.noreply.github.com>
2022-05-04 21:18:46 +0200

    Update UFO.java

--- UFO.java
+++ UFO.java
@@ -1,13 +1,16 @@
 public class UFO extends Character {
 
-
+	/** Constructor
+	  */
     public UFO (int xini,int yini){ 
         super(xini,yini);
         this.player = 2;
     }
 
+	/** checks if the UFO has won
+	  */
     public void winOrLose (int xHero, int yHero){
-        if(this.x==xHero && this.y==yHero){
+        if(this.x==xHero && this.y==yHero){ //the UFO has caught the Hero, they are on the same tile
             this.hasWon=true;
             System.out.println("The UFO has won!");
         }
