9440a939bdd94feaef78d594a3b36acd82fc2a8f
Eleonore12345 <77687565+Eleonore12345@users.noreply.github.com>
2022-05-04 21:14:40 +0200

    Update Ground.java

--- Ground.java
+++ Ground.java
@@ -1,13 +1,17 @@
 import java.awt.*;
 
 
-public class Ground extends Tile{
-	
+public class Ground extends Tile{ //mother class of WinningTile
+
+	/** Constructor
+	  */
 	public Ground(int x, int y , int height, int width){
 	super(x, y, height, width); 
 	}
 	
-	public void paint(Graphics g){
+	/** paint method
+	  */
+	public void paint(Graphics g){ //draws a orange square at the position of the ground
 		g.setColor(new Color (198, 140, 83));
 		g.fillRect(this.posX, this.posY, this.height, this.width);
 		g.setColor(new Color (135, 89, 44));
